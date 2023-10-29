1c98d12a8bf14b6a4b9bd931ea01a20802ade31f
xgordin <marcusvmca@gmail.com>
2023-10-28 23:41:27 -0300

    arquitetura inicial

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'be220-unisaPlay',
+  appName: 'be220-usinaPlay',
   webDir: 'www',
   server: {
     androidScheme: 'https'
